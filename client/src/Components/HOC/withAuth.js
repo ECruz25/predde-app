80c0002ce0e9c438aa4876971567a4790c3dc4b2
edwinscruz <escruz@caracolknits.com>
2019-10-03 12:21:22 -0600

    add withauth

--- client/src/Components/HOC/withAuth.js
+++ client/src/Components/HOC/withAuth.js
@@ -32,7 +32,7 @@ export default function withAuth(ComponentToProtect) {
         return null;
       }
       if (redirect) {
-        return <Redirect to="/login" />;
+        return <Redirect to="/editorial" />;
       }
       return (
         <React.Fragment>
